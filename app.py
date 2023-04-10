7902a98081086af6bf14a5a1f79d2f40fa356a9a
dhanyakanugrah <130309868+dhanyakanugrah@users.noreply.github.com>
2023-04-10 21:05:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ st.header('Enter 3 numbers')
 
 def user_input_features():
     
-    number_1 = st.number_input("first number",min_value=0,max_value=float(inf),step=1)
-    number_2 = st.number_input("second number",min_value=0,max_value=2000000**200000,step=1)
-    number_3 = st.number_input("third number",min_value=0,max_value=2000000**200000,step=1)
+    number_1 = st.number_input("first number",min_value=0,max_value=float('inf'),step=1)
+    number_2 = st.number_input("second number",min_value=0,max_value=float('inf'),step=1)
+    number_3 = st.number_input("third number",min_value=0,max_value=float('inf'),step=1)
 
     data = {'first number': number_1,
             'second number': number_2,
